8b555b989f1eff4df4643f344cb21315fc32e2b9
Ved-sketch <vedgupta1806@gmail.com>
2025-12-20 00:38:17 +0530

    added the sections.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-
+import logo from "../src/images/x_logo.png"
+import {Link} from "react-router"
 
 const App = () => {
   return (
     <div className="relative h-full w-full">
       <div className="flex">
-        <div className="flex-1 min-h-screen bg-red-500"></div>
-        <div className="flex-1 min-h-screen bg-blue-500"></div>
-        <div className="flex-1 min-h-screen bg-green-500"></div>
+        <div className="flex-3 min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800">
+
+              <div className="absolute">
+                <div className="m-2">
+                  <div >
+                    <Link to={"/"} className="btn btn-ghost mb-6 ">
+                      <img src={logo} alt="x_logo" className="w-16 h-16"/>
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="m-2">
+                  <div >
+                    <Link to={"/"} className="btn btn-ghost mb-6 ">
+                      {/* <img src={logo} alt="x_logo" className="w-16 h-16"/> */}
+                      Push me
+                    </Link>
+                  </div>
+                </div>
+              </div>
+        </div>
+
+        <div className="flex-4 min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800"></div>
+        <div className="flex-4 min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800"></div>
       </div>
     </div>
   )
